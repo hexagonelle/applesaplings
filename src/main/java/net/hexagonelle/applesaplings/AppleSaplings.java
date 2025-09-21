2bf7dc9b25d64687f31386ebddd2d27c9e6c66f5
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-09-20 19:13:39 -0700

    Revert "changed from alpha to beta"
    This reverts commit 5ba94f63f47c05c32d61bcec18db9f04358bc3c5.

--- src/main/java/net/hexagonelle/applesaplings/AppleSaplings.java
+++ src/main/java/net/hexagonelle/applesaplings/AppleSaplings.java
@@ -33,6 +33,9 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(AppleSaplings.MODID)
 public class AppleSaplings
+
+    //I like foxes
+
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "applesaplings";
