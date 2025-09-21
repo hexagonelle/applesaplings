df1223ffee8b94d184b782307722f48492a88c32
Elle <115524533+hexagonelle@users.noreply.github.com>
2025-09-20 19:17:00 -0700

    I like foxes
    Listen I beansed up my git and I'm trying to put it all back together again I promise this will make sense in the end

--- src/main/java/net/hexagonelle/applesaplings/AppleSaplings.java
+++ src/main/java/net/hexagonelle/applesaplings/AppleSaplings.java
@@ -35,6 +35,9 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(AppleSaplings.MODID)
 public class AppleSaplings
+
+  //I like foxes
+
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "applesaplings";
